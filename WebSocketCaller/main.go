03ad203305d4e186fec3d75c2114522f19b69f47
Sirok47 <75079540+Sirok47@users.noreply.github.com>
2021-05-28 12:38:26 +0300

    Add files via upload

---
+++ WebSocketCaller/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+	for {
+		time.Sleep(time.Second*3)
+		err := c.WriteMessage(websocket.TextMessage, []byte("kotorii chas?"))
+		if err != nil {
+			log.Println("write:", err)
+			return
+		}
+	}
+}
